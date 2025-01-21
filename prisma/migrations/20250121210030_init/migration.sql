967aa49ea32fcbb4b571a7140e5da7e12684151d
idmaksim <dementevmaksim1657@gmail.com>
2025-01-22 00:00:59 +0300

    add full url for media in db

--- prisma/migrations/20250121210030_init/migration.sql
+++ prisma/migrations/20250121210030_init/migration.sql
@@ -73,6 +73,7 @@ CREATE TABLE "post_medias" (
 CREATE TABLE "medias" (
     "id" TEXT NOT NULL,
     "url" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
     "type" "MediaType" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
