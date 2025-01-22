cae1726e172078fa25e35318adba31a7df5ff960
idmaksim <dementevmaksim1657@gmail.com>
2025-01-22 22:51:24 +0300

    add second user to seed func

--- prisma/seeds/user.seed.ts
+++ prisma/seeds/user.seed.ts
@@ -21,4 +21,26 @@ export const seedUser = async (prisma: PrismaClient) => {
       },
     },
   });
+
+  const group = await prisma.group.findFirst({});
+
+  await prisma.user.create({
+    data: {
+      email: 'string2@gmail.com',
+      firstName: 'string2',
+      lastName: 'string2',
+      group: {
+        connect: {
+          id: group.id,
+        },
+      },
+      password: '$2a$04$3EuFgtfrKleWTT2lHiioTOfk7d6r0vz.B.Scf4rKGEaJSQ6bfiFBi',
+      isActive: true,
+      role: {
+        connect: {
+          name: BaseRoleEnum.User,
+        },
+      },
+    },
+  });
 };
