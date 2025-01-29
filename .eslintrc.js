346ff7e39d0302ce48454640107a817e01cdaeed
idmaksim <dementevmaksim1657@gmail.com>
2025-01-29 18:06:16 +0300

    #refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/interface-name-prefix': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
